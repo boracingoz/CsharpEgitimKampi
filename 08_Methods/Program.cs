2f1bbbbe70a3492f5e004d8b8217d1adc9ddd82a
boracingoz26 <boracingoz@gmail.com>
2024-10-19 16:41:02 +0300

    Methods

---
+++ 08_Methods/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region  void Methods
+
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("Ali Yıldız");
+            //    Console.WriteLine("Ayşe Yıldız");
+            //    Console.WriteLine("Merve Merve");
+            //}
+
+            //CustomerList();
+
+            //void Sum()
+            //{
+            //    int x = 1;
+            //    int y = 2;
+            //    int z = x +y;
+
+            //    Console.WriteLine(z);   
+            //}
+
+            //Sum();
+
+
+
+            Console.Read();
+            #endregion
+
+            #region Geriye değer döndürmeyen parametreli metotlar
+
+            //void WriteMethod(string customerName)
+            //{
+            //    Console.WriteLine(customerName);
+            //}
+
+            //WriteMethod("Bora Cingöz");
+
+            //void CustomerCard(string name, string surname)
+            //{
+            //    Console.WriteLine("Müşteri: " + name + " " + surname);
+            //}
+
+            //CustomerCard("Bora", "Cingöz");
+
+            //Console.ReadKey(); 
+
+            //void Sum(int a, int b, int c) 
+            //{
+            //    int result = a + b + c;
+            //    Console.WriteLine(result);  
+            //}
+
+            //Sum(1, 2, 3);
+            //Console.ReadKey();
+            #endregion
+
+            #region Geriye Değer döndüren Methodlar
+
+            //string CustomerName()
+            //{
+            //    return "Bora Cingöz";
+            //}
+
+            //CustomerName();
+
+            //string StudentCard()
+            //{
+            //    string name = "Ali";
+            //    string Surname = "Yeşil";
+
+            //    return name + " " + Surname;    
+            //}
+
+            //Console.WriteLine(StudentCard());
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
